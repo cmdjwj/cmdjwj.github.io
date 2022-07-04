9ee78cf207184823c3c8c0ff7b71eb14eaa73b14
jwjawa <63486733+jwjawa@users.noreply.github.com>
2022-07-04 12:51:14 +0800

    update

--- updateblog.bat
+++ updateblog.bat
@@ -2,8 +2,9 @@
 title updateblog
 echo 请确认博客文件是否已经生成并确保博客文件夹与网站文件夹在同一目录下。
 pause
-rd blog /s /q
-move ..\jwjblog\public .\
-ren public blog
+rd /s /q blog
+xcopy /e /i /q ..\jwjblog\public .\blog
+echo 是否继续？
+pause
 update.bat
 pause
